cf8a3cb3486250981159ec72f8e0639e69d643f4
Nicolas Borges <nicolasborges736@gmail.com>
2026-04-21 19:18:16 -0300

    parte do borges pronta (html e js)

--- script.js
+++ script.js
@@ -23,8 +23,118 @@ function abrirConta() {
     window.location.href = "depositoSaque.html";
 }
 
-function carregarSaldo(){
-    let saldo = localStorage.getItem("saldo");
+function carregarSaldo() {
+    let saldo = pegarSaldo();
 
-    document.getElementById("saldo").innerText = saldo;
+    document.getElementById("saldo").innerText =
+        saldo.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+}
+
+function deposito() {
+
+    let saldo = pegarSaldo()
+
+    let valorAuteracao = document.getElementById('valorAuteracao').value;
+
+    valorAuteracao = valorAuteracao.replace(/[^0-9,]/g, '');
+
+    if (valorAuteracao === "") {
+        alert('Numero invalido ou ausente');
+        return;
+    }
+
+    valorAuteracao = valorAuteracao.replace(',', '.');
+
+    valorAuteracao = parseFloat(valorAuteracao);
+
+    if (isNaN(valorAuteracao)) {
+        alert('Numero invalido ou ausente');
+        return;
+    }
+
+    saldo = saldo + valorAuteracao;
+
+    localStorage.setItem("saldo", saldo);
+
+    document.getElementById("saldo").innerText =
+        saldo.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+
+    salvarHistorico("Depósito: +" + valorAuteracao.toFixed(2));
+
+    mostrarExtrato();
+    document.getElementById("valorAuteracao").value = "";
+}
+
+function saque() {
+
+    let saldo = pegarSaldo()
+
+    let valorAuteracao = document.getElementById('valorAuteracao').value;
+
+    valorAuteracao = valorAuteracao.replace(/[^0-9,]/g, '');
+
+    if (valorAuteracao === "") {
+        alert('Numero invalido ou ausente');
+        return;
+    }
+
+    valorAuteracao = valorAuteracao.replace(',', '.');
+
+    valorAuteracao = parseFloat(valorAuteracao);
+
+    if (isNaN(valorAuteracao)) {
+        alert('Numero invalido ou ausente');
+        return;
+    }
+
+    if (valorAuteracao > saldo) {
+        alert("Saldo insuficiente");
+        return;
+    }
+    saldo = saldo - valorAuteracao;
+
+    localStorage.setItem("saldo", saldo);
+
+    document.getElementById("saldo").innerText =
+        saldo.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+
+    salvarHistorico("Saque: -" + valorAuteracao.toFixed(2));
+
+    mostrarExtrato();
+    document.getElementById("valorAuteracao").value = "";
+}
+
+function pegarSaldo() {
+    return parseFloat(localStorage.getItem("saldo")) || 0;
+}
+
+function salvarHistorico(texto) {
+    let historico = JSON.parse(localStorage.getItem("historico")) || [];
+
+    historico.push(texto);
+
+    localStorage.setItem("historico", JSON.stringify(historico));
+}
+
+function mostrarExtrato() {
+    let lista = document.getElementById("extrato");
+
+    let historico = JSON.parse(localStorage.getItem("historico")) || [];
+
+    lista.innerHTML = "";
+
+    historico.forEach(item => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        lista.appendChild(li);
+    });
 }
